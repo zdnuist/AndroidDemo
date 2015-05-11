421f7ac262105fd401348fd161a88573d2293eac
zdnuist <619058238@qq.com>
2015-05-11 19:43:16 +0800

    add 屏幕亮度调节demo

--- src/me/zdnuist/android/MainActivity.java
+++ src/me/zdnuist/android/MainActivity.java
@@ -6,6 +6,7 @@ import me.zdnuist.android.demo3.AlarmManagerActivity;
 import me.zdnuist.android.demo4.Demo4Activity;
 import me.zdnuist.android.demo5.Demo5Activity;
 import me.zdnuist.android.demo6.Demo6Activity;
+import me.zdnuist.android.demo7.Demo7Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -22,6 +23,7 @@ public class MainActivity extends BaseActivity implements OnClickListener{
 		findViewById(R.id.btn_4).setOnClickListener(this);
 		findViewById(R.id.btn_5).setOnClickListener(this);
 		findViewById(R.id.btn_6).setOnClickListener(this);
+		findViewById(R.id.btn_7).setOnClickListener(this);
 	}
 
 	Intent intent;
@@ -51,6 +53,10 @@ public class MainActivity extends BaseActivity implements OnClickListener{
 			break;
 		case R.id.btn_6:
 			intent = new Intent(this,Demo6Activity.class);
+			startActivity(intent);
+			break;
+		case R.id.btn_7:
+			intent = new Intent(this,Demo7Activity.class);
 			startActivity(intent);
 			break;
 		}
